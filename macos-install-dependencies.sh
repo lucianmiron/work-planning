f04c3c754dab03c5815eda205b98837dbb026b1c
lucianmiron <mironlucianv@gmail.com>
2022-08-02 22:35:44 +0300

    mongo db connection

---
+++ macos-install-dependencies.sh
@@ -0,0 +1,3 @@
+#brew
+#mongodb
+#node
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.4')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
